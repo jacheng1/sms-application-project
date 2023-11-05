acc61a673a778ef289140e1777f961f9ee156be8
Jacky Cheng <75710146+jacheng1@users.noreply.github.com>
2023-11-04 21:59:24 -0700

    Add hamburger menu.

--- sms-frontend/src/App.js
+++ sms-frontend/src/App.js
@@ -1,38 +1,80 @@
 import messageLogo from './messageLogo.png';
-import { useState } from 'react';
+import Typography from '@mui/material/Typography';
+import Link from '@mui/material/Link';
+import Box from '@mui/material/Box';
+import Grid from '@mui/material/Grid';
+import TextField from '@mui/material/TextField';
+import Button from '@mui/material/Button';
+import Container from '@mui/material/Container';
+import { useEffect, useState } from 'react';
 import axios from 'axios';
 import './App.css';
 
+// Return copyright string
+function Copyright(props) {
+  return (
+    <Typography variant="body2" color="text.secondary" align="center" {...props}>
+      {'Copyright © '}
+      <Link color="inherit">
+        JMC SMS
+      </Link>{' '}
+      {new Date().getFullYear()}
+      {'.'}
+    </Typography>
+  );
+};
+
 function App() {
-  // memory storage
+  // Memory storage
   const [phoneNumber, setPhoneNumber] = useState("");
   const [message, setMessage] = useState("");
   const [error, setError] = useState(null);
   const [successfulResponse, setSuccessfulResponse] = useState(null);
 
+  // 1- Create state that will know if the overlay is opened or closed
+  const [isOverlayOpen, setIsOverlayOpen] = useState(false);
+
+  // Everytime a value changes, the content of the use effect is going to be
+  // evaluated
+  useEffect(() => {
+    // if the open
+    // update the overlay display to be visible
+    // if closed
+    // update the overlay to be none
+    if (isOverlayOpen) {
+      document.getElementById("overlay").style.display = "block";
+    }
+    else {
+      document.getElementById("overlay").style.display = "none";
+    }
+
+    console.log("isOverlayOpen is",isOverlayOpen);
+  }, [isOverlayOpen])
+
   const updatePhoneNumber = (e) => {
-    // output value that was typed into text field
+    // Output value that was typed into text field
     console.log({value: e.target.value});
 
-    // save input value into local memory
+    // Save input value into local memory
     setPhoneNumber(e.target.value);
   };
 
   const updateMessage = (e) => {
-    // output value that was typed into text field
+    // Output value that was typed into text field
     console.log({value: e.target.value});
 
-    // save input value into local memory
+    // Save input value into local memory
     setMessage(e.target.value);
   };
 
   const submitMessage = () => {
+    // Print confirmation message
     console.log("Send button was pressed");
 
     setError(null);
     setSuccessfulResponse(null);
 
-    // Collect data from the state, & send data to the backend
+    // Collect data from the state via parameters, & send data of parameters to the backend
     axios.post("http://localhost:3000/create-customer-text", {
       phoneNumber: phoneNumber, 
       message: message,
@@ -46,11 +88,28 @@ function App() {
     });
   };
 
+// When the X button is clicked I want to toggle the value
+// from false -> true ; true -> false
+// isOverlayOpen
   return (
     <html>
       <body className="body-container">
         {error && <div>ERROR: DATA WAS NOT SENT SUCCESSFULLY {error}</div>}
         {successfulResponse && <div>MESSAGE SUCCESSFULLY CREATED {successfulResponse}</div>}
+        <div id="overlay"></div>
+        <div className="hamburger-container">
+            <input class="checkbox" type="checkbox" onClick={() => setIsOverlayOpen(!isOverlayOpen)}/>
+            <div className="hamburger">
+              <span class="line line1"></span>
+              <span class="line line2"></span>
+              <span class="line line3"></span>
+            </div>
+          <div class="hamburger-items">
+            <li><a href="#">Home</a></li>
+            <li><a href="#">Messages</a></li>
+            <li><a href="#">Contacts</a></li>
+          </div>
+        </div>
         <div className="header-container">
           <h1>SMS Messenger</h1>
           <img src={messageLogo} alt="Message Logo" style={{width: "150px", height: "150px"}}></img>
@@ -58,28 +117,71 @@ function App() {
         <div className="main-container">
           <div className="field-container">
             <h2>Send a message!</h2>
-              <label for="pNumber">Phone Number</label>
-              <br></br>
-              <input type="text" id="pNumber" name="phoneNumber" placeholder="Enter a phone number..." value={phoneNumber} onChange={updatePhoneNumber}></input>
-
-              <br></br>
-
-              <label for="msg">Message</label>
-              <br></br>
-              <input type="text" id="msg" name="message" placeholder="Enter a message..." value={message} onChange={updateMessage}></input>
-
-              <br></br>
-
-              <input type="submit" id="sendButton" value="Send" onClick={() => submitMessage()}></input>
-      
-              <br></br>
-
-              <input type="submit" id="seeMessagesButton" value="View sent messages" ></input>
+            <Container component="main" maxWidth="xs">
+              <Box
+                sx={{
+                  display: 'flex',
+                  flexDirection: 'column',
+                  alignItems: 'center',
+                }}
+              >
+                <Box component="form" noValidate sx={{mt: 3}}>
+                  <Grid container spacing={2}>
+                    <Grid item xs={12}>
+                      <TextField
+                        required
+                        fullWidth
+                        variant="outlined"
+                        type="text"
+                        id="pNumber"
+                        label="Phone Number"
+                        name="phoneNumber"
+                        value={phoneNumber}
+                        onChange={updatePhoneNumber}
+                      />
+                    </Grid>
+                    <Grid item xs={12}>
+                      <TextField
+                        required
+                        fullWidth
+                        variant="outlined"
+                        type="text"
+                        id="msg"
+                        label="Message"
+                        name="message"
+                        value={message}
+                        onChange={updateMessage}
+                      />
+                    </Grid>
+                  </Grid>
+                  <Button
+                    fullWidth
+                    type="submit"
+                    id="sendButton"
+                    onClick={() => submitMessage()}
+                    variant="contained"
+                    sx={{mt: 3, mb: 2}}
+                  >
+                    Send
+                  </Button>
+                  <Button
+                    fullWidth
+                    type="submit"
+                    id="seeMessagesButton"
+                    variant="contained"
+                    sx={{mt: 3, mb: 2}}
+                  >
+                    View Sent Messages
+                  </Button>
+                </Box>
+              </Box>
+              <Copyright sx={{mt: 5}}/>
+            </Container>
           </div>
         </div>
       </body>
     </html>
   );
-}
+};
 
 export default App;
